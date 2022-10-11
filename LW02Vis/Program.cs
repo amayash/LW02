82444de731d157971eb1d2bb312e05035946ca68
dasha <amayashimizu89@gmail.com>
2022-10-12 01:38:04 +0400

    Восстановление

---
+++ LW02Vis/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LW02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kernel core = new();
+            Console.Write(core.GetListProcesses());
+            core.StartProcesses();
+            Console.WriteLine("all processes are ended");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LW02Vis/Thread.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace LW02
+{
+    internal class Thread
+    {
+        private int? time;
+        private Random random = new Random();
+        public bool End = false;
+        public Thread()
+        {
+            time = random.Next(0, 120);
+        }
+        public int Time { get { return (int)time; } set { time = value; } }
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            if (time != null)
+            {
+                s.Append(time);
+            }
+            return s.ToString();
+        }
+    }
+}
